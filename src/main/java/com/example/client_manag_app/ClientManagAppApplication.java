bafa501227a81aa6046ad966d12d934ae9073f41
Bruno-Fernandess <a71994@alunos.uminho,pt>
2021-07-02 10:02:04 +0100

    initial-commit

---
+++ src/main/java/com/example/client_manag_app/ClientManagAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.client_manag_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientManagAppApplication.class, args);
+    }
+
+}
