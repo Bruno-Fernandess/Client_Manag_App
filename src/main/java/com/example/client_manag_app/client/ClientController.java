869c698b6d44ca2b162ef4776f3ed86c662e2f2c
Bruno-Fernandess <a71994@alunos.uminho.pt>
2021-07-03 05:44:12 +0100

    controler-methods

--- src/main/java/com/example/client_manag_app/client/ClientController.java
+++ src/main/java/com/example/client_manag_app/client/ClientController.java
@@ -1,6 +1,7 @@
 package com.example.client_manag_app.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,44 @@ public class ClientController {
 
     @GetMapping("/clients")
     public List<Client> getAllClients() {
-        return (List<Client>) service.findAll();
+        return service.findAll();
     }
+
+    @GetMapping("/clients/{name}")
+    public ResponseEntity<List<Client>> findByName(@PathVariable("name") String name) {
+        List<Client> cls_name = service.getClientsWithName(name);
+        return new ResponseEntity<List<Client>>(cls_name, HttpStatus.OK);
+    }
+
+    @PostMapping("/clients/client")
+    public ResponseEntity<Client> createClient(@RequestBody Client cl) {
+        Client new_cl = service.save(cl);
+        return new ResponseEntity<Client>(new_cl, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/clients/client/{nif}")
+    public ResponseEntity<Client> getClientByNif(@PathVariable("nif") String nif) {
+        Client cl = service.getClientByNIF(nif);
+        return new ResponseEntity<Client>(cl, HttpStatus.OK);
+    }
+
+
+    @PutMapping("/clients/client/{id}")
+    public ResponseEntity<Client> updateClient(@PathVariable("id") Integer id, @RequestBody Client cl) {
+        Client up_cl = service.save(cl);
+        return new ResponseEntity<Client>(up_cl, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/clients/client/{id}")
+    public ResponseEntity<HttpStatus> deleteClient(@PathVariable("id") Integer id) {
+        service.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+
+
 }
