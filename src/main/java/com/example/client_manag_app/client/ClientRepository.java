4e3d3200762b62f3383ab07c57423d520982953c
Bruno-Fernandess <a71994@alunos.uminho.pt>
2021-07-03 04:11:00 +0100

    repository-methods

--- src/main/java/com/example/client_manag_app/client/ClientRepository.java
+++ src/main/java/com/example/client_manag_app/client/ClientRepository.java
@@ -2,5 +2,11 @@ package com.example.client_manag_app.client;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ClientRepository extends CrudRepository<Client,Integer> {
+
+public Client findClientByNif(String nif);
+public List<Client> findAllByNameContains(String name);
+
 }
