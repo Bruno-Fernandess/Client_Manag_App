12277c1e94a9cbaa1c5156914fdde0368c4d5dd1
Bruno-Fernandess <a71994@alunos.uminho.pt>
2021-07-04 09:36:41 +0100

    front-end-intg

--- src/main/java/com/example/client_manag_app/client/ClientController.java
+++ src/main/java/com/example/client_manag_app/client/ClientController.java
@@ -12,7 +12,6 @@ import java.util.List;
 //@RequestMapping("/api")
 public class ClientController {
 
-    //@Autowired ClientRepository repo;
     @Autowired ClientService service;
 
     @GetMapping("/clients")
